5c1d6596ed6e868efc9363606a75dbe45165ccdb
shouichi666 <hydra666kyo@gmail.com>
2025-05-25 17:45:09 +0900

    feat: config.hにタップ・ホールド判定と自動マウス操作の調整設定とコメントを追加

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_v1/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_v1/config.h
@@ -21,19 +21,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-//#    define RGBLIGHT_EFFECT_BREATHING
-//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_EFFECT_SNAKE
-//#    define RGBLIGHT_EFFECT_KNIGHT
-//#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//#    define RGBLIGHT_EFFECT_RGB_TEST
-//#    define RGBLIGHT_EFFECT_ALTERNATING
-//#    define RGBLIGHT_EFFECT_TWINKLE
+// #    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #    define RGBLIGHT_EFFECT_SNAKE
+// #    define RGBLIGHT_EFFECT_KNIGHT
+// #    define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #    define RGBLIGHT_EFFECT_RGB_TEST
+// #    define RGBLIGHT_EFFECT_ALTERNATING
+// #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
-#define TAP_CODE_DELAY 5
+// キーコードを送信する際のディレイ（ミリ秒単位）
+#define TAP_CODE_DELAY 0
 
+// タップとホールドの判定において、ホールドを強制する（素早いタップでもホールドと認識されやすくなる）
+#define TAPPING_FORCE_HOLD
+
+// タップとホールドの判定において、次のキー入力があった場合はホールドと判定する（早めにホールドを有効にする）
+#define PERMISSIVE_HOLD
+
+// タップとホールドを判定する時間の閾値（ミリ秒単位）
+// 188ms以内に離すとタップ、それ以降はホールドと判定される
+#define TAPPING_TERM 188
+
+// 自動マウス移動機能を有効にする（QMKのポイントデバイス機能の一部）
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+
+// 自動マウス機能を有効にするレイヤーを指定（1番レイヤーが対象になる）
 #define AUTO_MOUSE_DEFAULT_LAYER 1
