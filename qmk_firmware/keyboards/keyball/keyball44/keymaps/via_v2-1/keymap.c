50311361f86fddbc616c56a46546b65a5011f3d5
shouichi666 <hydra666kyo@gmail.com>
2025-06-29 19:49:04 +0900

    feat: ENETRキーをレイヤー２に移動

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_v2-1/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_v2-1/keymap.c
@@ -24,15 +24,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_0] = LAYOUT_universal(
         KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
-        KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , ENT_MO4   ,
+        KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT   ,
         KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_EQL    ,
                               KC_BTN2  , MO(4)  , KC_LALT  , TD(TD_EN_LGUI_LANG) , KC_SPC ,              BS_MO5 , JP_MO2,       RCTL_T(KC_LNG2) , KC_RALT  , LT(4, KC_ENT)
     ),
+    // [LAYER_0] = LAYOUT_universal(
+    //     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
+    //     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  ,    ,KC_QUOT
+    //     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_EQL    ,
+    //                           KC_BTN2  , MO(4)  , KC_LALT  , TD(TD_EN_LGUI_LANG) , KC_SPC ,              BS_MO5 , JP_MO2,       RCTL_T(KC_LNG2) , KC_RALT  , LT(4, KC_ENT)
+    // ),
 
     [LAYER_1] = LAYOUT_universal(
-        _______  , KC_QUES  , KC_7     , KC_8     , KC_9     , KC_AT    ,                                        KC_PERC     , KC_LPRN  , KC_UP     , KC_RPRN  , KC_LBRC  , KC_RBRC  ,
-        _______  , KC_EXLM  , KC_4     , KC_5     , KC_6     , KC_HASH  ,                                        KC_AMPR     , KC_LEFT  , KC_DOWN   , KC_RIGHT , KC_GRV   , _______  ,
-        _______  , KC_0     , KC_1     , KC_2     , KC_3     , KC_DLR   ,                                        KC_ASTR     , KC_QUOT  , _______   , _______  , _______  , KC_BSLS  ,
+        _______  , KC_QUES  , KC_7     , KC_8     , KC_9     , KC_AT    ,                                        KC_PERC     , KC_LPRN  , KC_UP     , KC_RPRN  , KC_LBRC  , KC_GRV  ,
+        _______  , KC_EXLM  , KC_4     , KC_5     , KC_6     , KC_HASH  ,                                        KC_AMPR     , KC_LEFT  , KC_DOWN   , KC_RIGHT , KC_RBRC  , KC_BSLS  ,
+        _______  , KC_0     , KC_1     , KC_2     , KC_3     , KC_DLR   ,                                        KC_ASTR     , KC_ENT   , _______   , _______  , _______  , _______  ,
                               _______  , _______  ,  _______ , _______ , _______ ,                       _______ , _______ ,       _______       , _______  , _______
     ),
 
